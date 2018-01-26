f4200644a97739d7888e8cf70fca16cd264a923b
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-01-26 14:33:04 -0500

    fixed Question 4

--- Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -10,8 +10,6 @@
  Write an `if` statement that prints "Must be Braeckman!" when the values of the constants would seem to suggest the person is Mr. Braeckman, or otherwise prints "Hmm, need more data to be sure."
  
  */
-
-
 let teachesPhysics = true
 let isMale = true
 let neverEverSarcastic = true
@@ -29,8 +27,6 @@ if teachesPhysics == true && isMale == true && neverEverSarcastic == false {
 } else {
     print("Hmm, need more data to be sure.")
 }
-
-
 
 /*:
  ## Now share your understanding
